7570d8d764d6e27621e1d24f18badcc9e5a6a16d
Konstantin K. <70386465+CuSynth@users.noreply.github.com>
2024-09-12 10:58:45 +0700

    GUI update

--- om_interface/OM_interface.py
+++ om_interface/OM_interface.py
@@ -10,17 +10,28 @@ from PyQt6.uic import loadUi
 # from GUI.generated.mainwindow_ui import Ui_MainWindow
 
     
+class SerialWidget(QtWidgets.QWidget):
 
-class Window(QMainWindow):
-    comboSelectPortA: QtWidgets.QComboBox
-    comboSpeedPortA: QtWidgets.QComboBox
-    
     def __init__(self):
         super().__init__()
+        ui_path: Path = Path(__file__).parent.joinpath('GUI', 'COM_set.ui')
+        loadUi(ui_path, self)
+
+
+
+class Window(QMainWindow):
+    widget_COM_port: QtWidgets.QFrame
+    layout_COM_port: QtWidgets.QVBoxLayout
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
         ui_path: Path = Path(__file__).parent.joinpath('GUI', 'mainwindow.ui')
         loadUi(ui_path, self)
         # self.setupUi(self)
         self.connectSignalsSlots()
+
+        self.COM_port_wgt = SerialWidget()
+        self.layout_COM_port.addWidget(self.COM_port_wgt)
 
     def connectSignalsSlots(self):
         self.action_About.triggered.connect(self.about)
